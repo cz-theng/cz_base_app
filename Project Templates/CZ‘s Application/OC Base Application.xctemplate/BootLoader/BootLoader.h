9c83ab9a454f7e7d9710dd60048f07aa3bba0cc8
C@Z <cz.theng@gmail.com>
2016-05-11 23:44:56 +0800

    tick

--- Project Templates/CZ‘s Application/OC Base Application.xctemplate/BootLoader/BootLoader.h
+++ Project Templates/CZ‘s Application/OC Base Application.xctemplate/BootLoader/BootLoader.h
@@ -1,5 +1,5 @@
 /*******************************************************************************\
-** tt:BootLoader.h
+** BootLoader.h
 ** Created by CZ(cz.devnet@gmail.com) on 5/11/16
 **
 **  Copyright © 2016 projm. All rights reserved.
--- Project Templates/CZ‘s Application/OC Base Application.xctemplate/BootLoader/BootLoader.m
+++ Project Templates/CZ‘s Application/OC Base Application.xctemplate/BootLoader/BootLoader.m
@@ -1,5 +1,5 @@
 /*******************************************************************************\
-** tt:BootLoader.m
+** BootLoader.m
 ** Created by CZ(cz.devnet@gmail.com) on 5/11/16
 **
 **  Copyright © 2016 projm. All rights reserved.
